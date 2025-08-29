a980719b74c429ef94817caff42d8e1099e98fe0
rogonza10 <l21210390@tectijuana.edu.mx>
2025-08-28 17:15:33 -0700

    Se agregaron animaciones

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text",{
+    strings:["Desarrollador Backend", "Desarrollador Web", "Ingeniero en Sistemas"],
+    typeSpeed:100,
+    backSpeed:100,       
+    backDelay:1000,
+    loop:true
+})
